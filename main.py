2479a82f2eca8d852998ab43e174ff939b4880ce
Luifer94 <91440075+Luifer94@users.noreply.github.com>
2021-11-22 09:32:35 -0500

    update de pythonanywhere

--- main.py
+++ main.py
@@ -1,22 +1,21 @@
 import os
 from flask.helpers import flash
 from werkzeug.utils import secure_filename
-from flask import flash, sessions
 from markupsafe import escape
-from forms.forms import *
-from flask import Flask, render_template, url_for, redirect, jsonify, request, session
-from db import *
-from utils import isUsernameValid, isEmailValid, isPasswordValid
+from forms.forms import Postform, LoginForm, UserForm
+from flask import Flask, render_template, url_for, redirect, request, session
+from db import get_db
+from utils import isEmailValid, isPasswordValid
 from werkzeug.security import generate_password_hash, check_password_hash
 from datetime import datetime
-import functools
 
-UPLOAD_FOLDER = os.path.abspath("static/images/Post")
+UPLOAD_FOLDER = os.path.abspath("/home/mlinan/Acuariofilia/static/images/Post")
+UPLOAD_FOLDER2 = os.path.abspath("/home/mlinan/Acuariofilia/static/images/Perfil")
 ALLOWED_EXTENSIONS = set(["png","jpg", "jpeg"])
 
 app = Flask(__name__)
 app.secret_key = os.urandom(24)
-app.config["UPLOAD_FOLDER"] = UPLOAD_FOLDER
+#app.config["UPLOAD_FOLDER"] = UPLOAD_FOLDER
 
 def allowed_file(filename):
     return "." in filename and filename.rsplit(".", 1)[1].lower() in ALLOWED_EXTENSIONS
@@ -26,7 +25,7 @@ def allowed_file(filename):
 #@login_required
 def feed():
     #@pablo@ esta condicion es por si alguien escribe directamente /feed en el navegador y no se encuentra logeado lo manda al login
-    #probando esto me di cuenta que le doy logout me manda al login, pero si escribo /feed puedo abrirlo porque no ha salido de la sesion. 
+    #probando esto me di cuenta que le doy logout me manda al login, pero si escribo /feed puedo abrirlo porque no ha salido de la sesion.
     if 'fullname' not in session:
         return redirect('login')
     else:
@@ -40,10 +39,11 @@ def feed():
         sql ="SELECT * FROM Post"
         posts = selectSQLite(sql)
         return render_template("feed.html", posts=posts, postown=postown)
-        
+
 
 @app.route('/addPost', methods=['GET', 'POST'])
 def addPost():
+    app.config["UPLOAD_FOLDER"] = UPLOAD_FOLDER
     if 'fullname' not in session:
         return redirect('login')
     else:
@@ -52,14 +52,38 @@ def addPost():
             nombre = request.form['nombre']
             imagen = request.files['imagen']
             filename = secure_filename(imagen.filename)
-            imagen.save(os.path.join(app.config["UPLOAD_FOLDER"], filename))     
+            imagen.save(os.path.join(app.config["UPLOAD_FOLDER"], filename))
             db = get_db()
             db.execute('INSERT INTO Post (PhotoURL,Title, creationDate, UserId) VALUES(?,?,?,?)', (filename,nombre, datetime.now(), session['id']))
             db.commit()
             mensajeExitoso ="Registro exitoso"
             return redirect(url_for('feed', mensajeExitoso=mensajeExitoso))
         return render_template('addPost.html', form=form)
-       
+
+@app.route('/addProfilePic', methods=['GET', 'POST'])
+def addProfilePic():
+    app.config["UPLOAD_FOLDER"] = UPLOAD_FOLDER2
+    up_query = ""
+    if 'fullname' not in session:
+        return redirect('login')
+    else:
+        form = Postform()
+        if request.method == 'POST':
+            imagen = request.files['imagen']
+            filename = secure_filename(imagen.filename)
+            imagen.save(os.path.join(app.config["UPLOAD_FOLDER"], filename))
+            db = get_db()
+            up_query = f"UPDATE User SET ProfilePicURL = '{filename}' WHERE UserId = '{session['id']}' ";
+            result = db.execute(up_query).rowcount
+            db.commit()
+            if result>0:
+                mensaje ="Imagen de perfil guardada exitosamente"
+            else:
+                mensaje= "Error al guardar imagen"
+            flash(mensaje)
+            return redirect(url_for('feed', mensajeExitoso=mensaje))
+        return render_template('Perfil.html', form=form)
+
 @app.route('/deleteComm', methods=['GET', 'POST'])
 def deleteComm():
     if 'fullname' not in session:
@@ -115,20 +139,18 @@ def postdetail():
         comPost= selectSQLite(sql)
         return render_template("postdetail.html", postsel=postsel,comPost=comPost)
 
-@app.route('/') 
+@app.route('/')
 @app.route('/login', methods=['GET', 'POST'])
 def login():
-
-    error = ""
     form = LoginForm()
-    if(form.validate_on_submit()):                
+    if(form.validate_on_submit()):
         email = escape(form.email.data.strip())
         password = escape(form.password.data.strip())
-        sql = f'SELECT * FROM User WHERE email = "{email}"'       
+        sql = f'SELECT * FROM User WHERE email = "{email}"'
         usuarios =selectSQLite(sql)
         if len(usuarios) > 0:
             contrasenaHas = usuarios[0][4]
-            if check_password_hash(contrasenaHas, password):            
+            if check_password_hash(contrasenaHas, password):
                 session.clear()
                 session['id'] = usuarios[0][0]
                 session['fullname'] = usuarios[0][1] + " " + usuarios[0][2]
@@ -137,15 +159,15 @@ def login():
                 session['password'] = contrasenaHas
                 session['foto'] = usuarios[0][9]
                 if session['rol'] == 2:
-                    return redirect(url_for('feed'))                                
+                    return redirect(url_for('feed'))
                 else:
                     return redirect(url_for('dashboard'))
             else:
-                flash(f'Usuario y/o Clave incorrecta')
+                flash('Usuario y/o Clave incorrecta')
                 return redirect(url_for('login'))
         else:
-                flash(f'Usuario y/o Clave incorrecta')             
-    return render_template("login.html", form=form)
+                flash('Usuario y/o Clave incorrecta')
+    return render_template("Login.html", form=form)
 
 
 @app.route('/logout')
@@ -159,16 +181,16 @@ def registro():
     form = UserForm()
     if request.method == 'POST':
         nombre = request.form['firstname']
-        apellido = request.form['lastname']            
+        apellido = request.form['lastname']
         email = request.form['email']
         password = request.form['password']
         birthday = request.form['Birthdate']
         gender = request.form['gender']
         role=2
         CreationDate= datetime.now()
-        
+
         #Si el correo es valido, si el correo existe, si el password es valido
-        sql = f'SELECT * FROM User WHERE email = "{email}"'   
+        sql = f'SELECT * FROM User WHERE email = "{email}"'
         usuarios = selectSQLite(sql)
         error = None
         if len(usuarios) > 0:  #si ya existe un usuario con ese email
@@ -186,7 +208,7 @@ def registro():
             return render_template("registro.html", form=form)
         else:
             contrasenaHas = generate_password_hash(password)
-            sql = 'INSERT INTO User (name,Lastname,email,password,gender,birthay,role,CreationDate) VALUES (?,?,?,?,?,?,?,?) '  
+            sql = 'INSERT INTO User (name,Lastname,email,password,gender,birthay,role,CreationDate) VALUES (?,?,?,?,?,?,?,?) '
             db = get_db()
             result = db.execute(sql, (nombre,apellido,email,contrasenaHas,gender,birthday,role,CreationDate)).rowcount
             db.commit()
@@ -205,7 +227,7 @@ def changepass():
         form = UserForm()
         if request.method == 'POST':
             currentPass = request.form['password']
-            newPass1 = request.form['newPass1']            
+            newPass1 = request.form['newPass1']
             newPass2 = request.form['newPass2']
 
             if not check_password_hash(session['password'], currentPass):    #si la clave hash de la sesion activa no coincide con la ingresada
@@ -227,7 +249,7 @@ def changepass():
                 db = get_db()
                 sql1 = f'UPDATE User SET password = ? WHERE email = ?'
                 result = db.execute(sql1, (newPassHash, session['correo'])).rowcount
-                db.commit()  
+                db.commit()
                 if result > 0:
                     flash('Se actualizó la contraseña exitosamente')
                 else:
@@ -248,7 +270,7 @@ def makeAdmin():
 
 @app.route('/restablecercontrasena')
 def restablecercontrasena():
-    return render_template("restablecercontrasena.html") 
+    return render_template("restablecercontrasena.html")
 
 @app.route('/editProfile', methods=['POST']) #ojo... debe haber siempre una sesion activa
 def editProfile():
@@ -256,7 +278,7 @@ def editProfile():
     flash(error)
     if 'id' in session:
         nombre = request.form['nombre']
-        apellido = request.form['apellido']            
+        apellido = request.form['apellido']
         birthday = request.form['nacimiento']
         #gender=request.form['genero']
         phone = request.form['telefono']
@@ -277,24 +299,23 @@ def dashboard():
     if session['rol']!=1:
         return redirect('feed')
     else:
-        return render_template("dashboard.html")      
+        return render_template("dashboard.html")
 
 @app.route('/search', methods=['GET', 'POST'])
 def search():
     if 'fullname' not in session:
         return redirect('login')
     else:
-        form = searchForm()
         if request.method == 'POST':
             clave = request.form['palabra']
-            sql = f'SELECT UserId,name, lastname, email,birthay,gender,CreationDate,ProfilePicURL FROM User WHERE name LIKE "%{clave}%" OR lastname LIKE "%{clave}%"'   
+            sql = f'SELECT UserId,name, lastname, email,birthay,gender,CreationDate,ProfilePicURL,about FROM User WHERE name LIKE "%{clave}%" OR lastname LIKE "%{clave}%"'
             Sresult = selectSQLite(sql)
-            sql = f'SELECT * FROM Post WHERE title LIKE "%{clave}%"'    
+            sql = f'SELECT * FROM Post WHERE title LIKE "%{clave}%"'
             posts = selectSQLite(sql)
             if len(Sresult) or len(posts) > 0:
-                return render_template("search.html", Sresult=Sresult, posts=posts)                               
+                return render_template("search.html", Sresult=Sresult, posts=posts)
             else:
-                flash(f'La busqueda no arrojo resultados')
+                flash('La busqueda no arrojo resultados')
                 return redirect(url_for('search'))
         if request.method == 'GET':
             return render_template("search.html")
@@ -304,7 +325,6 @@ def perfil():
     if 'fullname' not in session:
         return redirect('login')
     else:
-        form = UserForm()
         if request.method == 'GET':
             idsel = request.args.get('codigo')
             sql = f'SELECT * FROM User WHERE UserId = {idsel}'
@@ -312,7 +332,7 @@ def perfil():
             sql = f'SELECT * FROM User, Messages WHERE UserDe = {idsel} AND user.UserId=Messages.UserPara order by CreationDate desc limit 20'
             msgs = selectSQLite(sql)
             if len(msgs)==0:
-                msgs="vacio"           
+                msgs="vacio"
             return render_template("perfil.html", psel=psel[0], msgs=msgs)
 
 @app.route('/addcomm', methods=['GET','POST'])
@@ -338,9 +358,9 @@ def sendmes():
         time= datetime.now()
         sql=(f"INSERT INTO Messages (Content, UserPara, UserDe, CreationDate) VALUES('{contenido}',{psel},{de},'{time}')")
         db = get_db()
-        result=db.execute(sql)
+        db.execute(sql)
         db.commit()
-        
+
     return redirect(url_for('perfil', codigo=psel))
 
 
